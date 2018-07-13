191d08336f961ec7c9cdf2d4d98d230d19e6ddb9
benjamin-croker <benjamin.croker@gmail.com>
2018-07-13 20:30:17 +1000

    Format to pep8

--- portopt.py
+++ portopt.py
@@ -1,6 +1,7 @@
 import numpy as np
 import scipy.optimize as scipy_opt
 
+
 def portfolio_rets(rets, w):
     return np.matmul(rets, w)
 
@@ -22,6 +23,7 @@ def make_fun(rets, cov, q):
     """
     def f(w):
         return np.matmul(w, np.matmul(cov, w)) - q * np.matmul(rets, w)
+
     def df(w):
         return 2*np.matmul(cov, w) - q * rets
     return f, df
@@ -30,6 +32,7 @@ def make_fun(rets, cov, q):
 def constraint_fun(w):
     "returns 0 when the sum of W == 1"
     return w.sum() - 1
+
 
 def d_constraint_fun(w):
     return np.ones(w.shape)
@@ -41,8 +44,8 @@ def effecient_frontier(rets, cov, risk_free_rate,
     results = []
     # bound weights to +ve numbers (including 0)
     bounds = scipy_opt.Bounds(np.zeros(w0.shape), np.full(w0.shape, np.inf))
-    cons = [{'type':'eq', 'fun': constraint_fun, 'jac': d_constraint_fun}]
-    options = {'ftol':1e-16, 'disp': False}
+    cons = [{'type': 'eq', 'fun': constraint_fun, 'jac': d_constraint_fun}]
+    options = {'ftol': 1e-16, 'disp': False}
 
     for q in np.geomspace(min_q+1, max_q+1, num=n)-1:
         f, df = make_fun(rets, cov, q)
@@ -54,7 +57,7 @@ def effecient_frontier(rets, cov, risk_free_rate,
 
         if np.sum(np.abs(w0 - res['x'])) < stop_tol:
             break
-            
+
         p_rets = portfolio_rets(rets, res['x'])
         p_sd = portfolio_sd(cov, res['x'])
         results += [
@@ -75,7 +78,7 @@ def optim_sharpe(allocs, *args, **kwargs):
         kwargs['min_q'] = 0
     else:
         kwargs['min_q'] = allocs[i_min - 1]['q']
-        
+
     if i_min == len(allocs)-1:
         kwargs['max_q'] = 2 * allocs[i_min]['q']
     else:
